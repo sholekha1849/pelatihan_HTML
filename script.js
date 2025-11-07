e002b7eb8518c5951c49e2cf258a783aff451d13
sholekha1849 <sholekha.6@smk.belajar.id>
2025-11-06 17:51:59 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const tombolSapa = document.getElementById('sapaButton');
+tombolsapa.addEventListener('click',function(){
+
+                            alrt('halo!,Thanks Ya Udah Mampir!!);
+                                 });
